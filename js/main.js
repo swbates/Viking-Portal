2db4b6938487e41975375f62ab21ae9a6f0c2c0f
sbatesverso <34386785+sbatesverso@users.noreply.github.com>
2018-08-27 10:15:06 -0700

    init

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// If not iPhone, play first video and setup event handlers for  carousel rotations
+// iPhone will not play videos inline, and will take control of the browser
+$(document).ready(function(){
+  
+});
+
